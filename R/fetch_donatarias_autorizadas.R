a46e8e9bbefd1cc01686888568cbe72057b5831d
dar4ds <amievarodriguez.daniel@gmail.com>
2024-09-29 15:29:35 -0600

    Initializing gh-pages branch

---
+++ R/fetch_donatarias_autorizadas.R
@@ -0,0 +1,172 @@
+library(readxl)
+library(dplyr, warn.conflicts = FALSE)
+library(tidygeocoder)
+library(leaflet)
+
+# a function that downloads a file from url and reads it into a data frame and if succesfull delete the downloaded file
+fetch_donatarias_autorizadas <- function(url, sheet = 1, skip = 31) {
+  temp_file <- tempfile(fileext = ".xls")
+  download.file(url, temp_file)
+  donatarias_autorizadas <- read_xls(temp_file, sheet = sheet, skip = skip) |>
+    janitor::clean_names()
+  unlink(temp_file)
+  return(donatarias_autorizadas)
+}
+
+url_donatarias_autorizadas <- "http://omawww.sat.gob.mx/donatariasautorizadas/Paginas/documentos/padron_donatarias/DonatariasAutorizadas2024.xls"
+
+donatarias_autorizadas <- read_xls("DonatariasAutorizadas2024.xls", sheet = 1, skip = 31) |>
+  janitor::clean_names()#fetch_donatarias_autorizadas(url_donatarias_autorizadas)
+
+
+# Clean Up Dataset --------------------------------------------------------
+
+
+
+zoom_dona <- donatarias_autorizadas |>
+  select(entidad_federativa,
+         actividad_o_fin_autorizado,
+         denominacion_o_razon_social,
+         domicilio_fiscal,
+         objeto_social_autorizado,
+         e_mail
+  ) |>
+  mutate(
+    codigo_actividad_o_fin_autorizado = actividad_o_fin_autorizado,
+    categoria_actividad_o_fin_autorizado = case_when(
+      codigo_actividad_o_fin_autorizado == "A" ~ "Organizaciones civiles y fideicomisos asistenciales (artículo 79, fracción VI de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "B" ~ "Organizaciones civiles y fideicomisos educativos (artículo 79, fracción X de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "C" ~ "Organizaciones civiles y fideicomisos para la investigación científica o tecnológica (artículo 79, fracción XI de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "D" ~ "Organizaciones civiles y fideicomisos culturales (artículo 79, fracción XII de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "E" ~ "Organizaciones civiles y fideicomisos becantes (artículos 79, fracción XVII y 83 de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "F" ~ "Organizaciones civiles y fideicomisos ecológicos (artículo 79, fracción XIX de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "G" ~ "Organizaciones civiles y fideicomisos para la reproducción de especies en protección y peligro de extinción (artículo 79, fracción XX de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "H." ~ "Organizaciones civiles y fideicomisos de apoyo económico de donatarias autorizadas (artículo 82, penúltimo párrafo de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "I" ~ "Organizaciones civiles y fideicomisos para obras o servicios públicos (artículo 36, segundo párrafo del Reglamento de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "J" ~ "Organizaciones civiles y fideicomisos propietarios de bibliotecas privadas con acceso al público en general (artículo 134 del Reglamento de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "K" ~ "Organizaciones civiles y fideicomisos propietarios de museos privados con acceso al público en general (artículo 134 del Reglamento de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "L" ~ "Organizaciones civiles y fideicomisos de desarrollo social (artículo 79, fracción XXV de la Ley del ISR)",
+    codigo_actividad_o_fin_autorizado == "M" ~ "Organizaciones civiles y fideicomisos autorizados para recibir donativos deducibles en los términos del Convenio para Evitar la Doble Imposición e Impedir la Evasión Fiscal en Materia de Impuesto sobre la Renta, suscrito por el Gobierno de los Estados Unidos Mexicanos y el Gobierno de los Estados Unidos de América (artículo 82 de la Ley del Impuesto sobre la Renta, vigente a partir del 1 de enero de 2014, antes artículo 70-B y regla 3.10.7. de la Resolución Miscelánea Fiscal para 2024).",
+    TRUE ~ NA_character_  # Set to NA if none match
+  ),
+  short_categoria_actividad_o_fin_autorizado = case_when(
+    codigo_actividad_o_fin_autorizado == "A" ~ "Organizaciones civiles y caritativas",
+    codigo_actividad_o_fin_autorizado == "B" ~ "Instituciones educativas",
+    codigo_actividad_o_fin_autorizado == "C" ~ "Investigación y desarrollo",
+    codigo_actividad_o_fin_autorizado == "D" ~ "Instituciones culturales",
+    codigo_actividad_o_fin_autorizado == "E" ~ "Organizaciones de apoyo económico",
+    codigo_actividad_o_fin_autorizado == "F" ~ "Organizaciones ecológicas",
+    codigo_actividad_o_fin_autorizado == "G" ~ "Protección de especies",
+    codigo_actividad_o_fin_autorizado == "H" ~ "Entidades de apoyo económico",
+    codigo_actividad_o_fin_autorizado == "I" ~ "Entidades de servicio público",
+    codigo_actividad_o_fin_autorizado == "J" ~ "Bibliotecas privadas",
+    codigo_actividad_o_fin_autorizado == "K" ~ "Museos privados",
+    codigo_actividad_o_fin_autorizado == "L" ~ "Desarrollo social",
+    codigo_actividad_o_fin_autorizado == "M" ~ "Organizaciones autorizados para recibir donativos deducibles para evitar la Doble Imposición e Impedir la Evasión del ISR",
+    TRUE ~ NA_character_  # Establece NA si no coincide
+  ),
+  pseudo_categoria_actividad_o_fin_autorizado = case_when(
+    codigo_actividad_o_fin_autorizado %in% c("A", "B", "D", "E", "L") ~ "Organizaciones civiles y caritativas",
+    codigo_actividad_o_fin_autorizado == "B" ~ "Instituciones educativas",
+    codigo_actividad_o_fin_autorizado == "C" ~ "Investigación y desarrollo",
+    codigo_actividad_o_fin_autorizado %in% c("D", "J", "K") ~ "Instituciones culturales",
+    codigo_actividad_o_fin_autorizado %in% c("F", "G") ~ "Organizaciones ecológicas",
+    codigo_actividad_o_fin_autorizado == "H" ~ "Entidades de apoyo económico",
+    codigo_actividad_o_fin_autorizado == "I" ~ "Entidades de servicio público",
+    codigo_actividad_o_fin_autorizado == "M" ~ "Organizaciones con convenio México-EUA para Evitar la Doble Imposición e Impedir la Evasión Fiscal en ISR",
+    TRUE ~ NA_character_  # Establece NA si no coincide
+  )
+  ) |>
+  select(!actividad_o_fin_autorizado)
+
+zoom_dona |>
+  write.csv("augmented_donas_2024.csv", row.names = FALSE)
+
+
+# Geocode once Cause it takes 4 hours! ------------------------------------
+#FAILS
+# augmented_dona <- zoom_dona |>
+#   geocode(
+#     address = domicilio_fiscal,
+#     method = 'osm',
+#     lat = latitude ,
+#     long = longitude)
+
+
+# Test Leaflet ----------------------------------------------------------
+library(leaflet)
+
+zoom_dona
+# Example dataset
+states_data <- data.frame(
+  state = c("California", "Texas", "New York", "Florida"),
+  value = c(100, 200, 150, 175)
+)
+
+leaflet() |>
+  addTiles() |>
+  addPolygons(data = states_data, layerId = states_data$state)
+
+
+
+
+# Map ---------------------------------------------------------------------
+library(sf)
+library(ggplot2)
+library(osmdata)
+library(dplyr)
+
+# Define the bounding box for Mexico
+bbox_mexico <- c(-118.5, 14.5, -86.5, 32.5)
+
+# Use the bounding box in the opq function
+mexico_states_query <- opq(bbox = bbox_mexico) %>%
+  add_osm_feature(key = "boundary", value = "administrative") %>%
+  add_osm_feature(key = "admin_level", value = "4")
+
+
+# Get the data
+mexico_states <- osmdata_sf(mexico_states_query) |>
+  purrr::pluck("osm_multipolygons") |> #extract geometry
+  janitor::clean_names() |>
+  select(name,
+         iso3166_2,
+         geometry) |>
+  filter(
+    stringr::str_detect(iso3166_2, "MX")
+  )
+
+gc()
+
+library(plotly)
+base_map <- ggplot(mexico_states) +
+  geom_sf(
+          fill = "lightblue", color = "black") +
+  theme_void() +
+  ggtitle("States of Mexico")
+
+base_map
+
+plotly::ggplotly(base_map)
+
+library(mapview)
+#mapviewOptions(fgb = FALSE) # needed when creating web pages
+
+mexico_states |>
+  select(!iso3166_2) |>
+  mapview(col.regions = sf.colors(32), fgb = FALSE)
+
+library(leaflet)
+
+mexico_states |>
+  select(!iso3166_2) |>
+  leaflet()
+
+|>
+  addPolygons(color = "pink",
+              weight = 1,
+              smoothFactor = 0.5,
+              fillOpacity = 0.5,
+              fillColor = "purple",
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE))
---
+++ README.md
@@ -0,0 +1,24 @@
+# Quarto-Dona
+
+Pagina Web para Sencillamente Explorar Donatarias Autorizadas y una calculadora para estimar cuando se puede deducir de donaciones para personas fisicas en Mexico.
+
+## Herramientas
+
+ - [Quarto Live](https://github.com/r-wasm/quarto-live)
+
+
+## Features
+
+- [ ] calculator page for how much you can potentallly donate y hacer deducible de impuestos
+- [ ] pagina sobre "deducciones personales: focus on donaciones"
+- [ ] pagina donatarias autorizadas 2024 (cada cuanto se actualiza?)
+	- [ ] epoxy
+- [ ] pagina explore donatiaras por estado (interactive map )
+	- [ ] test new map packages
+- [ ] donatarias autorizadas attirbuts (reference sat original page)
+	- [ ] entidad federativa
+	- [ ] actividad o fin autorizado
+	- [ ] razon social/denominacion
+	- [ ] email
+	- [ ] domicilio fiscal
+	- [ ] objeto social autorizado
